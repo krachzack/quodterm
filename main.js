cb1acc71617373ab50145ca76114f70ea609dbff
Antonia Haslinger <antonia.haslinger@gmx.at>
2017-03-31 12:42:33 +0200

    [CHANGED] Starting html from index to startmenue.html It's not responsive for the prototype: Close the developer bar on the right to see correct size

--- main.js
+++ main.js
@@ -22,11 +22,11 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 1350, height: 720})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, 'startmenue.html'),
     protocol: 'file:',
     slashes: true
   }))
