a09262f3e140b1ee8229900673c53d3d05e93bfc
Antonia Haslinger <antonia.haslinger@gmx.at>
2017-03-31 14:35:01 +0200

    Working on UI Part 3

---
+++ question.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+var cd_interval;
+var countdown_start = 15;
+var countdown;
+var timer;
+var timer_startwidth;
+var timer_number;
+
+window.onload = function() {
+	countdown = countdown_start;
+	timer = document.getElementById("timer");
+	timer_startwidth = timer.offsetWidth;
+	timer_number = document.getElementById("timer_number");
+	cd_interval = setInterval(timedown, 1000);	
+};
+
+function timedown() {
+	countdown--;
+	timer.style.width = (timer_startwidth * countdown / countdown_start) + "px";
+	timer_number.innerHTML = countdown;
+	if (countdown <= 0) {
+		clearInterval(cd_interval);
+	}
+}
